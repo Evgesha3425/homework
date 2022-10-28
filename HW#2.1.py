890628835b7613e9b252a358bc6febf632b7f880
Yauheni <93941151+Evgesha3425@users.noreply.github.com>
2022-10-28 18:19:23 +0300

    Add hw
    update homework

--- HW#2.1.py
+++ HW#2.1.py
@@ -3,8 +3,6 @@
 какое из них наибольшее.
 """
 
-# Вариант 1 (Если вводить каждое число на отдельной строке)
-
 a, b, c = int(input()), int(input()), int(input())
 
 if a > b and a > c:
@@ -13,8 +11,3 @@ elif b > c and b > a:
     print(b)
 else:
     print(c)
-
-# Вариант 2 (Если вводить числа в одну строку)
-
-my_list = [i for i in input().split()]
-print(max(my_list))
